bf79130c28f38b5c7134d61435d2caadb21caf4b
Stanley Huang <Stanley.X.Huang@iag.com.au>
2020-10-05 11:23:55 +1100

    exercise 4, able to select squares

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -6,13 +6,16 @@ import React from 'react'
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
   const squares = Array(9).fill(null)
-
+  const [state, setState] = React.useState(squares)
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
   // - winner ('X', 'O', or null)
   // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
   // 💰 I've written the calculations for you! So you can use my utilities
   // below to create these variables
+  const nextValue = calculateNextValue(state)
+  const winner = calculateWinner(state)
+  const status = calculateStatus(winner, state, nextValue)
 
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
@@ -20,24 +23,30 @@ function Board() {
     // 🐨 first, if there's already winner or there's already a value at the
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
-    //
+    if (winner || state[square]) {
+      return
+    }
     // 🦉 It's typically a bad idea to manipulate state in React because that
     // can lead to subtle bugs that can easily slip into productions.
     // 🐨 make a copy of the squares array (💰 `[...squares]` will do it!)
+    const copyState = [...state]
     // 🐨 Set the value of the square that was selected
+
     // 💰 `squaresCopy[square] = nextValue`
-    //
+    copyState[square] = nextValue
     // 🐨 set the squares to your copy
+    setState(copyState)
   }
 
   function restart() {
     // 🐨 set the squares to `Array(9).fill(null)`
+    setState(Array(9).fill(null))
   }
 
   function renderSquare(i) {
     return (
       <button className="square" onClick={() => selectSquare(i)}>
-        {squares[i]}
+        {state[i]}
       </button>
     )
   }
